3dcd2a928356f85575bdbf707234e5de86390bea
OMartsin <oleksandrmartsin@gmail.com>
2024-03-11 16:58:51 +0200

    [fix] change variant

--- src/main/java/dev/martsin/MyFourierConfig.java
+++ src/main/java/dev/martsin/MyFourierConfig.java
@@ -5,14 +5,16 @@ import java.util.Map;
 
 public class MyFourierConfig {
 
+    //2,81; 2,95; 3,21; 3,39; 3,54; 3,76
+
     public static final Map<Double, Double> FUNCTION = new HashMap<>(){
         {
-            put(-Math.PI + 1 * ((2 * Math.PI) / 6), 4.42);
-            put(-Math.PI + 2 * ((2 * Math.PI) / 6), 5.41);
-            put(-Math.PI + 3 * ((2 * Math.PI) / 6), 5.09);
-            put(-Math.PI + 4 * ((2 * Math.PI) / 6), 7.33);
-            put(-Math.PI + 5 * ((2 * Math.PI) / 6), 8.05);
-            put(-Math.PI + 6 * ((2 * Math.PI) / 6), 9.87);
+            put(-Math.PI + 1 * ((2 * Math.PI) / 6), 2.81);
+            put(-Math.PI + 2 * ((2 * Math.PI) / 6), 2.95);
+            put(-Math.PI + 3 * ((2 * Math.PI) / 6), 3.21);
+            put(-Math.PI + 4 * ((2 * Math.PI) / 6), 3.39);
+            put(-Math.PI + 5 * ((2 * Math.PI) / 6), 3.54);
+            put(-Math.PI + 6 * ((2 * Math.PI) / 6), 3.76);
         }
     };
     public static final double A = -Math.PI;
